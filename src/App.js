b5b632fc197eb44363a8631c0190fd80e8ff4317
Usuario <cristiandavid.corrales@gmail.com>
2023-07-05 21:14:17 -0500

    Se crean funcionalidades de busqueda y conteo de tareas

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { CreateTodoButton } from "./CreateTodoButton";
 import { TodoTitle } from "./TodoTitle";
 
 const defaultTodos = [
-  { text: "Cortar cebolla", completed: true },
+  { text: "Cortar cebolla", completed: false },
   { text: "Tomar el curso de React.js", completed: false },
   { text: "Llorar un ratico", completed: false },
   { text: "Otro pendiente", completed: false },
@@ -16,14 +16,28 @@ const defaultTodos = [
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const completedTodos = todos.filter((todo) => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter((todo) => {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todoText.includes(searchText);
+  });
+
+  console.log("Los usuarios buscan todos de " + searchValue);
+
   return (
     <>
       <TodoContainer>
         <TodoTitle />
-        <TodoCounter completed={16} total={25} />
-        <TodoSearch />
+        <TodoCounter completed={completedTodos} total={totalTodos} />
+        <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
         <TodoList>
-          {defaultTodos.map((todo) => (
+          {searchedTodos.map((todo) => (
             <TodoItem
               key={todo.text}
               text={todo.text}
